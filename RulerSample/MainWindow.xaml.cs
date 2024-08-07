9da0506437b5539f57549cc05c0d93fe59760a9e
cwk <chaewk15@gmail.com>
2024-08-07 17:37:12 +0900

    Add project files.

---
+++ RulerSample/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RulerSample
+{
+    public partial class MainWindow : Window
+    {
+        private Point _startPoint;
+        private Ruler? _ruler;
+        private bool _isMoving = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawingCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _isMoving = true;
+            Point clickPosition = e.GetPosition(DrawingCanvas);
+
+            _startPoint = clickPosition;
+            _ruler = new Ruler()
+            {
+                Stroke = Brushes.Red,
+                StrokeThickness = 2,
+                X1 = _startPoint.X,
+                Y1 = _startPoint.Y,
+                X2 = _startPoint.X,
+                Y2 = _startPoint.Y
+            };
+            DrawingCanvas.Children.Add(_ruler);
+            DrawingCanvas.CaptureMouse();
+        }
+
+        private void DrawingCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isMoving && _ruler != null)
+            {
+                Point currentPosition = e.GetPosition(DrawingCanvas);
+                _ruler.X2 = currentPosition.X;
+                _ruler.Y2 = currentPosition.Y;
+            }
+        }
+
+        private void DrawingCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            DrawingCanvas.ReleaseMouseCapture();
+            _isMoving = false;
+            _ruler = null;
+        }
+    }
+}
---
+++ RulerSample/Ruler.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+public class Ruler : Shape
+{
+
+    // Dependency Property 
+    public static readonly DependencyProperty X1Property = DependencyProperty.Register(
+        "X1", typeof(double), typeof(Ruler), new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender));
+
+    public static readonly DependencyProperty Y1Property = DependencyProperty.Register(
+        "Y1", typeof(double), typeof(Ruler), new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender));
+
+    public static readonly DependencyProperty X2Property = DependencyProperty.Register(
+        "X2", typeof(double), typeof(Ruler), new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender));
+
+    public static readonly DependencyProperty Y2Property = DependencyProperty.Register(
+        "Y2", typeof(double), typeof(Ruler), new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsRender));
+
+    public static new readonly DependencyProperty StrokeThicknessProperty = DependencyProperty.Register(
+        "StrokeThickness", typeof(double), typeof(Ruler), new FrameworkPropertyMetadata(1.0, FrameworkPropertyMetadataOptions.AffectsRender));
+
+    // Property for Dependency Property
+    public double X1
+    {
+        get { return (double)GetValue(X1Property);}
+        set { SetValue(X1Property, value);}
+    }
+
+    public double Y1
+    {
+        get { return (double)GetValue(Y1Property);}
+        set { SetValue(Y1Property, value);}
+    }
+
+    public double X2
+    {
+        get { return (double)GetValue(X2Property);}
+        set { SetValue(X2Property, value);}
+    }
+
+    public double Y2
+    {
+        get { return (double)GetValue(Y2Property);}
+        set { SetValue(Y2Property, value);}
+    }
+
+    public new double StrokeThickness
+    {
+        get { return (double)GetValue(StrokeThicknessProperty);}
+        set { SetValue(StrokeThicknessProperty, value); }
+    }
+
+    protected override Geometry DefiningGeometry
+    {
+        get
+        {
+            LineGeometry geometry = new LineGeometry(new Point(X1, Y1), new Point(X2, Y2));
+            return geometry;
+        }
+    }
+
+    protected override void OnRender(DrawingContext drawingContext)
+    {
+        base.OnRender(drawingContext);
+
+        Pen pen = new Pen(Stroke, StrokeThickness);
+        drawingContext.DrawLine(pen, new Point(X1, Y1), new Point(X2, Y2));
+
+        double length = Math.Sqrt(Math.Pow(X2 - X1, 2) +  Math.Pow(Y2 - Y1, 2));
+        FormattedText formattedText = new FormattedText(
+            length.ToString("F2") + " px",
+            System.Globalization.CultureInfo.InvariantCulture,
+            FlowDirection.LeftToRight,
+            new Typeface("Arial"),
+            12,
+            Brushes.Black,
+            VisualTreeHelper.GetDpi(this).PixelsPerDip);
+
+        Point midpoint = new Point((X1 + X2) / 2, (Y1 + Y2) / 2);
+        drawingContext.DrawText(formattedText, new Point(midpoint.X + 5, midpoint.Y + 5));
+
+        DrawTicks(drawingContext, pen);
+    }
+
+    private void DrawTicks(DrawingContext drawingContext, Pen pen)
+    {
+        double interval = 10.0;
+        double tickLength = 10.0;
+
+        Vector lineVector = new Vector(X2-X1, Y2 - Y1);
+        double lineLength = lineVector.Length;
+        lineVector.Normalize();
+
+        // 수직벡터
+        Vector perpendicularVector = new Vector(-lineVector.Y, lineVector.X);
+
+        for (double i = 0; i < lineLength; i += interval)
+        {
+            Point tickPoint = new Point(X1, Y1) + lineVector * i;
+            Point tickStart = tickPoint - perpendicularVector * tickLength / 2;
+            Point tickEnd = tickPoint + perpendicularVector * tickLength / 2;
+
+            drawingContext.DrawLine(pen, tickStart, tickEnd);
+        }
+    }
+}
